cb48004be5c2ab05841dc9e64cdb0c95440327dc
Juhi Shyamsukha <js3527@cornell.edu>
2023-12-08 17:26:27 -0500

    Update prompt_engineering.py

--- prompt_engineering.py
+++ prompt_engineering.py
@@ -10,7 +10,7 @@ def engineering_prompt(resume, job_description, section):
   """ Produce a prompt for an LLM to generate a resume section that matches the given job description"""
   prompt = "temporary prompt"
   if section == "Education":
-    pass
+    prompt = "Given the following resume: "+ resume + "write an Education section that includes the degree type, B.S., M.S. as well the university the years they attended and the location of that university" 
   elif section == "Skills":
     pass
   elif section == "Professional Experience":
