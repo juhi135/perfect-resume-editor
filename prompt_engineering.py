9c745b0573d6f05013745af41f3476d32187385b
Jessica Ip <jvi5@cornell.edu>
2023-12-11 16:09:05 -0500

    Edited prompt_engineering.py

--- prompt_engineering.py
+++ prompt_engineering.py
@@ -8,15 +8,16 @@ PREVIOUS_EXPERIENCE_PROMPT = ""
 
 def engineering_prompt(resume, job_description, section):
   """ Produce a prompt for an LLM to generate a resume section that matches the given job description"""
-  prompt = "temporary prompt"
+  prompt = "Given the following resume: " + resume 
   if section == "Education":
-    prompt = "Given the following resume: "+ resume + "write an Education section that includes the degree type, B.S., M.S. as well the university the years they attended and the location of that university" 
+    prompt += "Write an Education section that includes the degree type, B.S., M.S. as well as the university, the years they attended, and the location of that university" 
   elif section == "Skills":
-    pass
+    prompt += "Edit the skills section to more closely match this job section: " + section 
   elif section == "Professional Experience":
-    pass
+    prompt += "Edit the professional experience to align more closely with this job description" + section + " using the STAR method" 
   elif section == "Projects":
-    pass
+    prompt += "Edit the project descriptions to match the responsibilities of the job section more closely"
   # add more sections here
 
+  #check 1024 word count ?
   return prompt # SHOULD BE JSON
