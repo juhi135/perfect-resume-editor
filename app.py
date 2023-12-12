cf10ad31acd48c68286229c31f1ac15f1234cb61
Juhi Shyamsukha <juhi.shyamsukha@gmail.com>
2023-12-11 20:41:57 -0500

    added openai setup

--- app.py
+++ app.py
@@ -6,69 +6,69 @@ import mimetypes
 import docx
 from PyPDF2 import PdfReader
 from prompt_engineering import engineering_prompt
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import openai
 
 import requests
 
 # need to run pip install transformers==4.20  
 
-# def process_pdf_to_str(file_path):
-#     """ Convert pdf input to string """
-#     with open(file_path, 'rb') as f:
-#             pdf_reader = PdfReader(f)
-#             pdf_text = ""
-#             # Iterate through all pages and extract text
-#             for page_num in range(len(pdf_reader.pages)):
-#                 page = pdf_reader.pages[page_num]
-#                 pdf_text += page.extract_text()
-#     return pdf_text
+def process_pdf_to_str(file_path):
+    """ Convert pdf input to string """
+    with open(file_path, 'rb') as f:
+            pdf_reader = PdfReader(f)
+            pdf_text = ""
+            # Iterate through all pages and extract text
+            for page_num in range(len(pdf_reader.pages)):
+                page = pdf_reader.pages[page_num]
+                pdf_text += page.extract_text()
+    return pdf_text
 
-# def process_docs_to_str(file_path):
-#     """ Convert docs inputs to string """
-#     doc = docx.Document(file_path)
-#     doc_text = ""
-#     for paragraph in doc.paragraphs:
-#         doc_text += paragraph.text + "\n"
-#     return doc_text
+def process_docs_to_str(file_path):
+    """ Convert docs inputs to string """
+    doc = docx.Document(file_path)
+    doc_text = ""
+    for paragraph in doc.paragraphs:
+        doc_text += paragraph.text + "\n"
+    return doc_text
 
-# # Function to process the uploaded file and convert it to JSON
-# def process_file_to_str(uploaded_file):
-#     """ Takes in the uploaded file and returns a string """
-#     temp_dir = 'tempDir'
-#     os.makedirs(temp_dir, exist_ok=True)
-#     file_path = os.path.join(temp_dir, uploaded_file.name)
+# Function to process the uploaded file and convert it to JSON
+def process_file_to_str(uploaded_file):
+    """ Takes in the uploaded file and returns a string """
+    temp_dir = 'tempDir'
+    os.makedirs(temp_dir, exist_ok=True)
+    file_path = os.path.join(temp_dir, uploaded_file.name)
     
-#     with open(file_path, 'wb') as f:
-#         f.write(uploaded_file.getvalue())
+    with open(file_path, 'wb') as f:
+        f.write(uploaded_file.getvalue())
 
-#     # Detect file type and decode accordingly
-#     file_extension = uploaded_file.name.split('.')[-1].lower()
+    # Detect file type and decode accordingly
+    file_extension = uploaded_file.name.split('.')[-1].lower()
 
-#     found_text = ""
+    found_text = ""
 
-#     if file_extension == 'pdf':
-#     # Read the contents of the PDF file
-#         found_text=process_pdf_to_str(file_path)
+    if file_extension == 'pdf':
+    # Read the contents of the PDF file
+        found_text=process_pdf_to_str(file_path)
 
-#     elif file_extension == 'docx':
-#         # Read the contents of the DOCX file
-#         found_text=process_docs_to_str(file_path)
+    elif file_extension == 'docx':
+        # Read the contents of the DOCX file
+        found_text=process_docs_to_str(file_path)
 
-#     elif file_extension == 'txt':
-#         # Read the contents of the TXT file
-#         with open(file_path, 'r', encoding='utf-8') as f:
-#             txt_text = f.read()
-#         found_text=txt_text
+    elif file_extension == 'txt':
+        # Read the contents of the TXT file
+        with open(file_path, 'r', encoding='utf-8') as f:
+            txt_text = f.read()
+        found_text=txt_text
 
-#     elif file_extension == 'json':
-#         # Read the contents of the JSON file
-#         with open(file_path, 'r', encoding='utf-8') as f:
-#             json_data = json.load(f)
-#             found_text=json.dumps(json_data)
-#     else:
-#         st.warning(f"Unsupported file type: {file_extension}")
+    elif file_extension == 'json':
+        # Read the contents of the JSON file
+        with open(file_path, 'r', encoding='utf-8') as f:
+            json_data = json.load(f)
+            found_text=json.dumps(json_data)
+    else:
+        st.warning(f"Unsupported file type: {file_extension}")
 
-#     return found_text
+    return found_text
 
 
 def parse_text_to_json(text):
@@ -82,22 +82,19 @@ Welcome to PRG! Drop your previous CV below, select one of the templates, and le
 """)
 
 # File uploader allows user to add their own document
-#uploaded_file = st.file_uploader("Choose a file", type=['pdf', 'docx', 'txt', 'json'])
+uploaded_file = st.file_uploader("Choose a file", type=['pdf', 'docx', 'txt', 'json'])
 
-resume_section = ""
 job_description = ""
 
-resume_section_area = st.text_area(label="Current Resume Section", height=200)
 job_description_area = st.text_area(label="Job Description", height=200)
 
-if st.button("Submit Resume & Job Description"):
-    resume_section = resume_section_area.strip()
+if st.button("Submit Job Description"):
     job_description = job_description_area.strip()
 
-    if resume_section and job_description:
-        st.write("Resume & Job Description saved successfully!")
+    if job_description:
+        st.write("Job Description saved successfully!")
     else:
-        st.write("Please fill in both sections before submitting.")
+        st.write("Please fill in the section before submitting.")
 
 # Sections
 sections = ["Education", "Skills", "Professional Experience", "Projects"]
@@ -108,18 +105,18 @@ selected_section = st.selectbox("Choose an option:", sections)
 # Display selected option
 st.write(f"You selected: {selected_section}")
 
-# if uploaded_file is not None:
-#     # Save the uploaded file to a temporary directory
-#     resume = process_file_to_str(uploaded_file)
+if uploaded_file is not None:
+    # Save the uploaded file to a temporary directory
+    resume_section = process_file_to_str(uploaded_file)
 
     # Assuming there is a function to process the uploaded file and generate resume
     # processed_file = process_file(uploaded_file)
     # st.download_button(label="Download Resume", data=processed_file, file_name="resume.pdf", mime='application/octet-stream')
 
-    # # For demonstration, we simply display the file name and details
-    # st.write("Filename:", uploaded_file.name)
-    # st.write("File type:", uploaded_file.type)
-    # st.write("File size:", uploaded_file.size)
+    # For demonstration, we simply display the file name and details
+    st.write("Filename:", uploaded_file.name)
+    st.write("File type:", uploaded_file.type)
+    st.write("File size:", uploaded_file.size)
 
     # If you want to use the uploaded file in other parts of the app, you might want to save it temporarily
     # Save the uploaded file locally for further processing
@@ -127,6 +124,24 @@ st.write(f"You selected: {selected_section}")
     #     f.write(uploaded_file.getbuffer())
 
     # st.success("File Saved")
+
+# Check if OpenAI API key is set as an environment variable
+if "OPENAI_API_KEY" not in os.environ:
+    # Display a message and input field for user to enter their key
+    st.info("OpenAI API key not found. Please enter your key:")
+    api_key = st.text_input("OpenAI API Key:")
+
+    # Check if the user submitted a key
+    if api_key:
+        # Set the environment variable with the provided key
+        os.environ["OPENAI_API_KEY"] = api_key
+        st.success("OpenAI API key set.")
+    else:
+        # Stop execution if no key is provided
+        st.error("Please enter a valid OpenAI API key.")
+        st.stop()
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 # Sidebar for navigation or additional settings
 with st.sidebar:
@@ -139,37 +154,22 @@ with st.sidebar:
 
     # When clicked will generate new resume section
     if st.button('Render JSON Resume'):
-        resume_section = resume_section_area.strip()
         job_description = job_description_area.strip()
-        print(resume_section)
-        print(job_description)
         prompt = engineering_prompt(resume_section, job_description, selected_section)
         print("Prompt: "+ prompt)
 
-        # Model and tokenizer
-        model_name = "gpt2-medium"
-        model = AutoModelForCausalLM.from_pretrained(model_name)
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-        # Prompt
-        #prompt = "Who ate the cat"
-
-        # Encode the prompt
-        encoded_prompt = tokenizer(prompt, return_tensors="pt")
-
-        # length of prompt
-        words_len = len(prompt.split())
-
-        # Generate text
-        generated_text = model.generate(**encoded_prompt, max_length=500, num_return_sequences=1, temperature=0.7, top_k=40, no_repeat_ngram_size=3)
-
-        # Decode the generated text
-        decoded_text = tokenizer.decode(generated_text[0])
-
-        print(f"Question: {prompt}")
-        print(f"Answer: {decoded_text}")
-
-        print("------- ANSWER OVER ----------------")
+        # Function to generate resume section
+        def generate_resume_section(prompt):
+            response = openai.Completion.create(
+                model="gpt-3.5-turbo",
+                prompt=prompt,
+                temperature=0.4, # higher values like 0.8 make the output mode random, range 0-2
+            )
+            return response["choices"][0]["text"]
+        
+        answer = generate_resume_section(prompt)
+        st.write("This is the generated resume section: "+ answer)
+        
         
 
 
--- prompt_engineering.py
+++ prompt_engineering.py
@@ -1,28 +1,53 @@
-import json
-
-EDUCATION_PROMPT = "prompt"
-
-SKILLS_PROMPT = ""
-
-PREVIOUS_EXPERIENCE_PROMPT = ""
 
 def engineering_prompt(resume, job_description, section):
   """ Produce a prompt for an LLM to generate a resume section that matches the given job description"""
   prompt = "Given the following resume: " + resume 
   if section == "Education":
-    prompt += "Write an Education section that includes the degree type, B.S., M.S. as well as the university, the years they attended, and the location of that university" 
+    prompt = """
+        Given the following resume"""+ resume + """. Generate an education section in the format
+        university/college: 
+        location: 
+        Degree and Degree type: 
+        start - end date:
+        GPA: 
+
+        To best match this job description:
+        """ + job_description
+
   elif section == "Skills":
-    prompt += "Edit the skills section to more closely match this job description: " + job_description
+    prompt = """
+        Given the following resume""" + resume + """. Generate a skills section in the which are
+        most relevant according to the job description: """ + job_description + """. Don't add 
+        skills which are not in the resume, pick skills from the work experience in the
+        resume section as well as from the projects. Focus on a top few set of skills.
+        """
+
+
   elif section == "Professional Experience":
-    prompt += "Edit the professional experience to align more closely with this job description" + job_description + " using the STAR method" 
+    prompt = """
+        Given the following resume"""+ resume + """. Generate a Professional experience section that
+        includes the experiences of the resume in the format
+        company:
+        position:
+        start - end date:
+        location:
+        summary:
+        The summary section should contain bullet points for what the person did during
+        the job. It should follow the STAR method. Highlight specific experiences and tailor
+        to match the following job description: """ + job_description
+
+
+
   elif section == "Projects":
-    prompt += "Edit the project descriptions to match the responsibilities of the job section more closely. Where the job description is: " + job_description
+    prompt = """
+      Given the following resume""" + resume + """. Generate a Projects section in the following format
+      Project Name:
+      Project start and end date:
+      explanation:
+      The explanation section should be done so that skills used in the project and project
+      accomplishments are highlighted. Only include projects from the resume. Use
+      an active voice, be gramatically correct, use correct spellings. Tailor this 
+      section to best fit the following job description: """ + job_description
+    
 
-  #check 1024 word count ?
-  words = prompt.split()
-  # Take the first max_words words
-  truncated_words = words[:1000]
-  # Join the words back into a string
-  truncated_prompt = ' '.join(truncated_words)
-
-  return truncated_prompt
+  return prompt
